2f8479eee21f03fa01698b136c1ea6770c14f819
Exedice <except.dice@gmail.com>
2024-02-11 17:51:24 +0200

    basic layout for the student view

---
+++ src/components/Header.svelte
@@ -0,0 +1,74 @@
+<script lang="ts">
+
+    function showSettings() {
+        // TODO
+    }
+
+    function logout() {
+        // TODO
+    }
+
+</script>
+
+<main>
+
+    <div class="left">
+
+        <h1>LearnHub</h1>
+
+    </div>
+
+    <div class="right">
+        
+        <button title="Settings" on:click={showSettings}>
+            <img src="./icons/settings.svg" alt="">
+        </button>
+
+        <button title="Log out" on:click={logout}>
+            <img src="./icons/exit.svg" alt="">
+        </button>
+
+        <p>Sussus Amongus</p>
+
+
+    </div>
+
+</main>
+
+<style>
+
+    main {
+        display: flex;
+        align-items: stretch;
+        height: 100px;
+        border-bottom: 3px solid var(--cl-divider);
+        box-sizing: border-box;
+    }
+
+    .left {
+        flex-grow: 1;
+        display: flex;
+        align-items: center;
+        margin-left: 100px;
+    }
+    
+    .right {
+        flex-grow: 1;
+        display: flex;
+        flex-direction: row-reverse;
+        gap: 55px;
+        align-items: center;
+        margin-right: 100px;
+    }
+
+    h1 {
+        font-size: 36px;
+        font-weight: 300;
+    }
+
+    p {
+        font-size: 31px;
+        font-weight: 300;
+    }
+
+</style>
--- src/routes/student/overview/StudentOverview.svelte
+++ src/routes/student/overview/StudentOverview.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     
+    import Header from "../../../components/Header.svelte";
     import TopBar from "../../../components/TopBar.svelte";
     import FeedContent from "./feed/FeedContent.svelte";
     import HomeworkContent from "./homework/HomeworkContent.svelte";
@@ -9,18 +10,31 @@
 
 <main>
 
-    <h1>Student overview page</h1>
+    <Header />
 
-    <TopBar />
+    <div class="content">
 
-    <!-- <FeedContent /> -->
+        <div></div>
 
-    <!-- <HomeworkContent /> -->
+        <TopBar />
+
+        <div style="width: 313px; background: red;"></div>
+
+        <FeedContent />
+
+    </div>
 
 </main>
 
 <style>
-
     
+    .content {
+        display: grid;
+        grid-template-columns: max-content max-content;
+        grid-template-rows: max-content max-content;
+        row-gap: 15px;
+        column-gap: 20px;
+        margin: 0 249px 0 249px;
+    }
 
 </style>
